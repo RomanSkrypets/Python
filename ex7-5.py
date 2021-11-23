56372fb3cc9d7151623d4b2faf71de245c2e1f2e
Roman Skrypets <roman.skripets2015@gmail.com>
2021-11-24 01:12:08 +0200

    Fixing code

--- ex7-5.py
+++ ex7-5.py
@@ -5,11 +5,11 @@ while True:
     age = int(input(cinema))
     if age == "exit":
         break
-    elif age < str(3):
-        print("Your ticket is free")
-    elif age >= str(3) and age < str(12):
-        print("Your ticket cost 10$")
-    elif age > str(12):
-        print("Your ticket cost 15$")
+    elif int(age) < int(3):
+        print("Your ticket is free\t")
+    elif int(age) >= int(3) and int(age) < int(12):
+        print("Your ticket cost 10$\t")
+    elif int(age) >= int(12):
+        print("Your ticket cost 15$\t")
     
     
